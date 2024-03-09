28b3ca33280ef4d042f7c29dadfc5042bd7e4fa6
347Online | Katie Janzen <katiejanzen@347online.me>
2024-03-09 17:22:53 -0600

    Misc changes
    WIP volume keys
    
    Adjusting neovim settings to use with homemanger

--- configuration.nix
+++ configuration.nix
@@ -9,30 +9,48 @@
   nixpkgs.config.allowUnfree = true;
 
   users.users.katie = {
+    shell = pkgs.nushell;
     isNormalUser = true;
     extraGroups = [ "wheel" ];
     packages = with pkgs; [
     ];
   };
 
+  fonts.enableDefaultPackages = true;
+
   environment = {
     variables = {
-      EDITOR = "nvim";
-      VISUAL = "nvim";
+      # NIXOS_OZONE_WL = "1";
+      GDK_SCALE = "2";
     };
 
     systemPackages = with pkgs; [
-      neovim
+      neofetch
       firefox
-      tree
       nushell
       git
       vscodium
       webcord
+      kitty
+      home-manager
+      eza
+      waybar
+      mako
+      libnotify
+      rofi
     ];
   };
 
   programs = {
+    neovim = {
+      enable = true;
+      defaultEditor = true;
+    };
+    fish.enable = true;
+    hyprland = {
+      enable = true;
+      xwayland.enable = true;
+    };
     _1password.enable = true;
     _1password-gui = {
       enable = true;
@@ -47,14 +65,17 @@
       enable = true;
       xkb.layout = "us";
       displayManager = {
-        sddm.enable = true;
+        sddm = {
+	  enable = true;
+	  wayland.enable = true;
+	};
         autoLogin = {
           enable = true;
           user = "katie";
         };
       };
 
-      desktopManager.plasma5.enable = true;
+      # desktopManager.plasma5.enable = true;
       libinput = {
         enable = true;
         touchpad = {
@@ -72,20 +93,23 @@
 
   # Asahi-specific
   hardware.asahi.useExperimentalGPUDriver = true;
-  programs.light.enable = true;  
+  programs.light.enable = true;
   services.actkbd = {
     enable = true;
     bindings = [
       { keys = [ 225 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -A 10"; }
       { keys = [ 224 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -U 10"; }
+      { keys = [ 113 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/amixer set Master toggle"; }
+      { keys = [ 114 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/runuser -l katie -c 'amixer -q set Master 5%- unmute'"; }
+      { keys = [ 115 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/runuser -l katie -c 'amixer -q set Master 5%+ unmute'"; }
     ];
   };
 
   boot.loader.systemd-boot.enable = true; 
   boot.loader.efi.canTouchEfiVariables = false;
 
-  time.timeZone = "America/Chicago";
   sound.enable = true;
+  time.timeZone = "America/Chicago";
   hardware.bluetooth.enable = true;
   networking.hostName = "Anathema";
   networking.wireless.iwd = {
