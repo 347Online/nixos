b600739dbaa7fb7aa9c454acbc94f8f09fd0f171
347Online | Katie Janzen <katiejanzen@347online.me>
2024-03-16 23:44:04 -0500

    Use a flake

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Katie's NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "aarch64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+
+      config = {
+        allowUnfree = true;
+      };
+    };
+  in {
+    nixosConfigurations = {
+      myNixos = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit system;};
+        modules = [
+          ./configuration.nix
+        ];
+      };
+    };
+  };
+}
